54309cf264b036443eb6b62f6f8685d55a1f4e09
jonasXchen <jonas@metacamp.so>
2023-06-05 17:06:46 +0800

    updated .env.example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
   import PageSelector from '$lib/sets/PageSelector.svelte'
+  import { Keypair } form '@solana/web3.js'
+  
+  const keypair = Keypair.fromSecretKey(
+  bs58.decode(
+    "5MaiiCavjCmn9Hs1o3eznqDEhRwxo7pXiAYez7keQUviUkauRiTMD8DrESdrNjN8zd9mTmVhRvBJeg5vhyvgrAhG"
+  )
+  );
+  console.log(Keypair)
 
 </script>
 
@@ -7,5 +15,8 @@
 <div class="flex justify-center">
 
   <PageSelector />
+
+
+
 
 </div>
