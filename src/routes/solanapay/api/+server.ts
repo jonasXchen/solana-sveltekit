6bb3ef9a14ce9d69c4eac7336fe2251db404398c
jonasXchen <jonas@metacamp.so>
2023-06-05 18:24:52 +0800

    updates solanapay environment variables

--- src/routes/solanapay/api/+server.ts
+++ src/routes/solanapay/api/+server.ts
@@ -3,13 +3,13 @@ import { Connection, Keypair, PublicKey, Transaction } from '@solana/web3.js';
 
 import { createSplTransferIx } from '$lib/utils/createSplTransferIx'
 import { json } from '@sveltejs/kit';
+import { PUBLIC_HTTPS_RPC_ENDPOINT, PUBLIC_USDC_DEV_MINT, PUBLIC_MERCHANT_WALLET } from '$env/static/public'
+import { PRIVATE_LOCAL_KEYPAIR } from '$env/static/private'
 
-import * as dotenv from 'dotenv'
-dotenv.config()
-const connection = new Connection(`${process.env.HTTPS_RPC_ENDPOINT}`, 'confirmed')
-const splToken = new PublicKey(process.env.USDC_DEV_MINT as String);
-const MERCHANT_WALLET = new PublicKey(process.env.MERCHANT_WALLET as String);
-const LOCAL_KEYPAIR = Uint8Array.from(process.env.LOCAL_KEYPAIR! as unknown as number[])
+const connection = new Connection(`${PUBLIC_HTTPS_RPC_ENDPOINT}`, 'confirmed')
+const splToken = new PublicKey(PUBLIC_USDC_DEV_MINT as String);
+const MERCHANT_WALLET = new PublicKey(PUBLIC_MERCHANT_WALLET as String);
+const LOCAL_KEYPAIR = Uint8Array.from(PRIVATE_LOCAL_KEYPAIR! as unknown as number[])
 
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
