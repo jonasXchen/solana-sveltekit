dd7a3b060029cf0f9ea830eca078f07ad66dd4b6
jonasXchen <jonas@metacamp.so>
2023-06-05 17:08:33 +0800

    remove boiler code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,5 @@
 <script>
   import PageSelector from '$lib/sets/PageSelector.svelte'
-  import { Keypair } form '@solana/web3.js'
-  
-  const keypair = Keypair.fromSecretKey(
-  bs58.decode(
-    "5MaiiCavjCmn9Hs1o3eznqDEhRwxo7pXiAYez7keQUviUkauRiTMD8DrESdrNjN8zd9mTmVhRvBJeg5vhyvgrAhG"
-  )
-  );
-  console.log(Keypair)
 
 </script>
 
