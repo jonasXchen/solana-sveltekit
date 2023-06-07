d99e71c8dac98d734635b904d1bc0b0d55d931dd
jonasXchen <jonas@metacamp.so>
2023-06-07 10:02:12 +0800

    v0.1

--- src/lib/sets/PageSelector.svelte
+++ src/lib/sets/PageSelector.svelte
@@ -11,16 +11,13 @@
 <section class="text-black dark:text-white" >
     <div class="items-center space-y-2">
         <div>
-            <Button label='Solana Operations with Token2022' onClick={() => goto('/solana')}/>
+            <Button label='Explore Solana Operations with Token2022' onClick={() => goto('/solana')}/>
         </div>
         <div>
-            <Button label='SolanaPay QR' onClick={() => goto('/solanapay')}/>
+            <Button label='Create SolanaPay QR' onClick={() => goto('/solanapay')}/>
         </div>
         <div>
-            <Button label='SolanaPay Endpoint' onClick={() => goto('/solanapay/api')}/>
-        </div>
-        <div>
-            <Button label='Solend Pay2Earn (WIP)' onClick={() => goto('/pay2earn')}/>
+            <Button label='Check SolanaPay Endpoint' onClick={() => goto('/solanapay/api')}/>
         </div>
     </div>
 
