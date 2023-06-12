3f05d7e622c05179e88ebe08b994b5f6aa03ad78
jonasXchen <jonas@metacamp.so>
2023-06-12 15:37:24 +0800

    Used Select component for SetCluster.svelte

--- src/lib/sets/SetCluster.svelte
+++ src/lib/sets/SetCluster.svelte
@@ -1,47 +1,26 @@
 <script lang=ts>
     import { cluster, connectedCluster } from "$lib/stores";
 	import { type Cluster, Connection, clusterApiUrl } from "@solana/web3.js";
-
-
-	// Define list for cluster options
-	let cluster_options = [
-		{ name: 'devnet'},
-		{ name: 'testnet'},
-		{ name: 'mainnet-beta'},
-		{ name: 'localnet'}
-	]
-	let clusters = [ 'devnet', 'testnet', 'mainnet-beta', 'localnet']
+	import Select from '../components/Select.svelte'
 
 	// Input/Output variables used
-	let cluster_selected : Cluster | 'localnet' = 'devnet'
+	let clusters = [ 'devnet', 'testnet', 'mainnet-beta', 'localnet']
+	let clusterSelected : Cluster | 'localnet' = 'devnet'
 	let clusterUrl : string
 
     // Set cluster connection via store
 	function handleSubmit() {
 
-		// Set new cluster and connection in store
-		cluster.set(
-			cluster_selected as Cluster
-		)
+		// Set new cluster in 
+		cluster.set(clusterSelected as Cluster)
 
-		if (cluster_selected == 'localnet') {
-			clusterUrl = 'http://127.0.0.1:8899'
-		} else {
-			clusterUrl = clusterApiUrl(cluster_selected as Cluster)
-		}
-		
-		connectedCluster.set(
-			// new Connection('https://rpc.helius.xyz/?api-key=72d348d8-4037-4fe1-a4d3-fe9e271be940', 'confirmed')
-			new Connection(clusterUrl, 'confirmed') 
-			// new Connection('https://devnet.genesysgo.net/') 
-		)
+		// Handle localnet
+		if (clusterSelected == 'localnet') { clusterUrl = 'http://127.0.0.1:8899'} 
+		else { clusterUrl = clusterApiUrl(clusterSelected as Cluster)}
+		// Set connection in store
 
+		connectedCluster.set(new Connection(clusterUrl))
 	}
-
-
-
-	handleSubmit()
-
 </script>
 
 
@@ -49,13 +28,6 @@
 <section class="text-black dark:text-white" >
 	<!-- User Input -->
 	<div>
-		<label for="Cluster">Cluster:</label>
-		<select class='text-black' bind:value={cluster_selected} on:change="{handleSubmit}">
-			{#each cluster_options as cluster}
-				<option class='text-black' value={cluster.name}>
-					{cluster.name}
-				</option>
-			{/each}
-		</select>
+		<Select label="Cluster" options={clusters} bind:bindValue={clusterSelected} onChange={handleSubmit} />
 	</div>
 </section>
