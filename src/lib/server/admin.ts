dcff707eeba722d66e6d0d1babce40c2b99288cf
jonasXchen <jonas@metacamp.so>
2023-09-03 18:35:21 +0800

    elasticsearch

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,3 +1,4 @@
 export let admins: string[] = [
-    "JonasQ6kwFknJKQpVXbAs2d3fdVLy2DnXd13ynwhgV4"
+    "JonasQ6kwFknJKQpVXbAs2d3fdVLy2DnXd13ynwhgV4",
+    "4JnxErTCHv6r9f913Mt9tthJfHVMDLZj7K4MWo8FfanD"
 ]
